7ab97f01cf4cd462f220f504a795297fbecfc0a2
ianlcz <yann.lecoz@ynov.com>
2020-01-14 00:13:16 +0100

    UPDATE 14/01/20
    DONE:
    Mise en place du projet
    *  Installation des dépendances d'Electron
    *  Création du fichier main.js avec les bonnes configurations
    *  Création du fichier index.html qui affiche un texte

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ianMovies
+
+## Auteur
+
+**Yann LE COZ** - Bordeaux Ynov Campus Informatique - [ianlcz](https://github.com/ianlcz)
+
+## Licence
+
+Ce programme est  sous licence [MIT](./LICENSE)
---
+++ main.js
@@ -0,0 +1,81 @@
+const electron = require('electron')
+const { app, BrowserWindow, Menu} = require('electron')
+
+process.env.NODE_ENV = 'development';
+
+let mainWindow;
+
+function createWindow() {
+  // Création de la fenêtre du navigateur
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // Chargement du fichier index.html de l'application
+  mainWindow.loadFile('index.html')
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+
+  // Construction du menu à partir du modèle
+  const mainMenu = Menu.buildFromTemplate(modelMenu)
+
+  // Insertion du menu
+  Menu.setApplicationMenu(mainMenu)
+}
+app.on('ready', createWindow)
+
+// Quitte l'application quand toutes les fenêtres sont fermées.
+app.on('window-all-closed', () => {
+  // Sur macOS, il est commun pour une application et leur barre de menu
+  // de rester active tant que l'utilisateur ne quitte pas explicitement avec Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+app.on('activate', () => {
+  // Sur macOS, il est commun de re-créer une fenêtre de l'application quand
+  // l'icône du dock est cliquée et qu'il n'y a pas d'autres fenêtres d'ouvertes.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+// Création du modèle de menu
+const modelMenu =  [
+  {
+    label: 'File',
+    submenu:[
+      {
+        label: "Quitter l'application",
+        accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+// Ajout des outils de développement si on développe l'application
+if(process.env.NODE_ENV !== 'production'){
+  modelMenu.push({
+    label: 'Développement',
+    submenu:[
+      {
+        label: 'Rafraîchir',
+        role: 'reload'
+      },
+      {
+        label: 'Inspecter les éléments',
+        accelerator:process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      }
+    ]
+  });
+}
